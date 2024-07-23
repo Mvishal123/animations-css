3402da58ba2637705df281ebfac38b00bc7873cb
vishal <vishalmahesh.study@gmail.com>
2024-07-23 09:52:06 +0530

    fix: alignment

--- advanced-stuff/frontend_system_design/file_system/src/App.tsx
+++ advanced-stuff/frontend_system_design/file_system/src/App.tsx
@@ -2,7 +2,7 @@ import FileSystem from "./components/file-system";
 
 const App = () => {
   return (
-    <div className="flex justify-center mt-24">
+    <div className="ml-60 mt-24">
       <FileSystem />
     </div>
   );
